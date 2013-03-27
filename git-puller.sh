4f6d8eec2ed887846b826d5f4bf4904bc7da195f
Radek Dolewa <R.Dolewa@gmail.com>
2013-03-27 23:36:43 +0100

    @
    @

--- git-puller.sh
+++ git-puller.sh
@@ -1,4 +1,3 @@
 cd /var/www/dev/Offline && /usr/lib/git-core/git pull && cd -
-cd /var/www/dev/Lemonbyte && /usr/lib/git-core/git pull && cd -
 cd /var/www/dev/internetdreams && /usr/lib/git-core/git pull && cd -
 cd /var/www/dev/GitPuller && /usr/lib/git-core/git pull && cd -
