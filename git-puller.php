333b7abb7deef5bf94e436426564748c6239586e
Radek Dolewa <R.Dolewa@gmail.com>
2013-03-27 22:42:57 +0100

    @First Commit
    Gitpuller added / Webhook

---
+++ git-puller.php
@@ -0,0 +1,30 @@
+<?php #!/usr/bin/env /usr/bin/php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+set_time_limit(0);
+ 
+try {
+ 
+  $payload = json_decode($_REQUEST['payload']);
+ 
+}
+catch(Exception $e) {
+ 
+    //log the error
+    file_put_contents('/var/www/dev/Offline/logs/github.txt', $e . ' ' . $payload, FILE_APPEND);
+ 
+      exit(0);
+}
+ 
+if ($payload->ref === 'refs/heads/master') {
+ 
+    $project_directory = '/var/www/dev/Offline/';
+ 
+    $output = shell_exec("/var/www/dev/git-puller.sh");
+ 
+    //log the request
+    file_put_contents('/var/www/dev/Offline/logs/github.txt', $output, FILE_APPEND);
+ 
+}
+
+?>
---
+++ git-puller.sh
@@ -0,0 +1,3 @@
+cd /var/www/dev/Offline && /usr/lib/git-core/git pull && cd -
+cd /var/www/dev/Lemonbyte && /usr/lib/git-core/git pull && cd -
+cd /var/www/dev/internetdreams && /usr/lib/git-core/git pull && cd -
