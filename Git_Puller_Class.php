a98518fe1a747e2af07b0cc498b178e3fb063acc
Radek Dolewa <R.Dolewa@gmail.com>
2013-04-03 23:49:21 +0200

    @
    @

---
+++ Git_Puller_Class.php
@@ -0,0 +1,24 @@
+<?php 
+
+function start_sh(){
+	$output = shell_exec("/var/www/dev/git-puller.sh");
+	return $output;
+}
+
+function server_log($output){
+	file_put_contents('/var/www/dev/GitPuller/logs/github.txt', $output, FILE_APPEND);
+}
+
+function mail_log($output){
+	$to      = 'r.dolewa@gmail.com';
+	$subject = 'Gitpuller Script';
+	$message = $output;
+	$headers = 'From: Gitpuller@Lemonbyte.nl' . "\r\n" .
+			'Reply-To: Gitpuller@Lemonbyte.nl' . "\r\n" .
+			'X-Mailer: PHP/' . phpversion();
+
+	mail($to, $subject, $message, $headers);
+}
+
+
+?>
--- git-puller.php
+++ git-puller.php
@@ -2,6 +2,8 @@
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
 	set_time_limit(0);
+	
+	include("Git_Puller_Class.php");
 	 
 	try {
 	 
@@ -24,24 +26,6 @@
 	
 	}
 
-	function start_sh(){
-		    $output = shell_exec("/var/www/dev/git-puller.sh");
-		    return $output;
-	}
 
-	function server_log($output){
-			file_put_contents('/var/www/dev/GitPuller/logs/github.txt', $output, FILE_APPEND);
-	}
-	
-	function mail_log($output){
-			$to      = 'r.dolewa@gmail.com';
-			$subject = 'Gitpuller Script';
-			$message = $output;
-			$headers = 'From: Gitpuller@Lemonbyte.nl' . "\r\n" .
-				'Reply-To: Gitpuller@Lemonbyte.nl' . "\r\n" .
-				'X-Mailer: PHP/' . phpversion();
-
-			mail($to, $subject, $message, $headers);
-	}
 	
 ?>
