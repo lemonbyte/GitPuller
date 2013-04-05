b04b82032218a5d30bfec42f330f962f1b318d24
Radek Dolewa <r.dolewa@readview.nl>
2013-04-05 16:36:46 +0200

    xxx
    xx

--- git-puller.php
+++ git-puller.php
@@ -6,9 +6,14 @@
 	include("Git_Puller_Class.php");
 	 
 	try {
-	 
 		$payload = json_decode($_REQUEST['payload']);
-	 
+		//$payload = '{ "after": "ef54f864d067f792edf9782e35d786daeb8a18e0", "before": "17100784be8693da6822a66dc9f1d47b32b74b38", "commits": [ { "added": [], "author": { "email": "r.dolewa@readview.nl", "name": "Radek Dolewa", "username": "dolewa" }, "committer": { "email": "r.dolewa@readview.nl", "name": "Radek Dolewa", "username": "dolewa" }, "distinct": true, "id": "ef54f864d067f792edf9782e35d786daeb8a18e0", "message": "sa\n\ndsadsa", "modified": [ "git-puller.php" ], "removed": [], "timestamp": "2013-04-05T06:17:02-07:00", "url": "https://github.com/lemonbyte/GitPuller/commit/ef54f864d067f792edf9782e35d786daeb8a18e0" } ], "compare": "https://github.com/lemonbyte/GitPuller/compare/17100784be86...ef54f864d067", "created": false, "deleted": false, "forced": false, "head_commit": { "added": [], "author": { "email": "r.dolewa@readview.nl", "name": "Radek Dolewa", "username": "dolewa" }, "committer": { "email": "r.dolewa@readview.nl", "name": "Radek Dolewa", "username": "dolewa" }, "distinct": true, "id": "ef54f864d067f792edf9782e35d786daeb8a18e0", "message": "sa\n\ndsadsa", "modified": [ "git-puller.php" ], "removed": [], "timestamp": "2013-04-05T06:17:02-07:00", "url": "https://github.com/lemonbyte/GitPuller/commit/ef54f864d067f792edf9782e35d786daeb8a18e0" }, "pusher": { "email": "r.dolewa@gmail.com", "name": "lemonbyte" }, "ref": "refs/heads/master", "repository": { "created_at": 1364420452, "fork": false, "forks": 0, "has_downloads": true, "has_issues": true, "has_wiki": true, "id": 9064286, "language": "PHP", "master_branch": "master", "name": "GitPuller", "open_issues": 0, "owner": { "email": "r.dolewa@gmail.com", "name": "lemonbyte" }, "private": false, "pushed_at": 1365167893, "size": 492, "stargazers": 0, "url": "https://github.com/lemonbyte/GitPuller", "watchers": 0 } }';
+ 	 	//var_dump(json_decode($payload));
+
+		//var_dump($payload->commits[0]->author);
+	 	//die();
+	 	
+		
 	}
 	catch(Exception $e) {
 	
@@ -20,11 +25,7 @@
  	
 	if ($payload->ref === 'refs/heads/master') {
 		
-		$payload_data = $payload->committer['email'];
-		//"<br>".$payload->modified.$payload->removed.$payload->timestamp.$payload->committer;
-		
-		$payload = json_decode($_REQUEST['payload']);
-		
+		$payload_data = 'Payload Data';
 		$shelldata = start_sh();
 		mail_log($shelldata,$payload_data);
 		server_log($shelldata);
